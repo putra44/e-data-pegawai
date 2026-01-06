39850b59d73630a864efabe05bafb02a9982b565
putra44 <44189041+putra44@users.noreply.github.com>
2026-01-07 02:32:27 +0900

    Update logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -1,21 +1,11 @@
 <?php
 session_start();
 
-/*
- | Logout
- | - Set notifikasi logout sukses
- | - Hapus session
- | - Redirect ke login
-*/
-
-// flash message
 $_SESSION['logout_success'] = true;
 
-// hapus session login
 session_unset();
 session_destroy();
 
-// start ulang session khusus flash
 session_start();
 $_SESSION['logout_success'] = true;
 
